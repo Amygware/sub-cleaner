828d9367bc4fbf6262cdb9351d990c182cec3b77
iGlody <ilukaspesek@gmail.com>
2025-03-23 00:33:50 +0100

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import '../app.css';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { onMount } from 'svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
 	let { children } = $props();
+
+	onMount(() => {
+		inject({ mode: dev ? 'development' : 'production' });
+	});
 </script>
 
 <div class="bg-base-100 min-h-screen" data-theme="light">
